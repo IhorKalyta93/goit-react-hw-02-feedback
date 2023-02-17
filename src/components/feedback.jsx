1781b80b423e2c1a2aebe7534b6f4f22c0fcdf77
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-02-17 13:56:10 +0100

    1.1

--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -1,23 +1,60 @@
-import React from "react";
-import styled from './Feedback.module.css'
-export
-class Feedback extends React.Component {
-
-    state = {
-        value: 5
-    }
-handleIncrement =() =>{
-    this.setState({value:10})
+import React from 'react';
+import styled from './Feedback.module.css';
+export class Feedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleIncrementGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  handleIncrementNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleIncrementBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+  render() {
+    return (
+      <div className={styled.mainDiv}>
+        <h1>Please leave feedback</h1>
+        <div className={styled.divResults}>
+          <span className={styled.span}>Good: {this.state.good}</span>
+          <span className={styled.span}>Neutral: {this.state.neutral}</span>
+          <span className={styled.span}>Bad: {this.state.bad}</span>
+        </div>
+        <div className={styled.divButtons}>
+          <button
+            className={styled.button}
+            type="button"
+            onClick={this.handleIncrementGood}
+          >
+            Good
+          </button>
+          <button
+            className={styled.button}
+            type="button"
+            onClick={this.handleIncrementNeutral}
+          >
+            Neutral
+          </button>
+          <button
+            className={styled.button}
+            type="button"
+            onClick={this.handleIncrementBad}
+          >
+            Bad
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-    render() {
-        return (
-            <div className={styled.mainDiv}>
-                <span>{this.state.value}</span>
-                <div><button type="button" onClick={this.handleIncrement}>Добавить</button></div>
-            </div>
-        )
-    }
-    
-};
-;
 export default Feedback;
